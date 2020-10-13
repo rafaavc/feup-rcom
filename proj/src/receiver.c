f032111a9673f87516c93aa363c32ebedbd34e64
Jeff <jaquelim@pirocadas.com>
2020-10-13 12:42:57 +0100

    Second class done

--- proj/src/receiver.c
+++ proj/src/receiver.c
@@ -1,6 +1,7 @@
 #include "protocol.h"
 
 extern int logicConnectionFlag;
+extern enum stateMachine state;
 
 int main(int argc, char** argv){
     int fd;
@@ -14,23 +15,29 @@ int main(int argc, char** argv){
 
     fd = openConfigureSP(argv[1], &oldtio);
 
-    //unsigned valid = FALSE, counter = 0;
+    unsigned valid = FALSE;
+// counter = 0;
     char* ret;
     char* buf = NULL;
     ssize_t res, size;
     buf = constructSupervisionMessage(ADDR_SENT_RCV, CTRL_UA);
 
-
+    state = Start;
     //tries to read the message back from the serialPort
     ret = readFromSP(fd, &size,0);
-    
+    printf("Read from sp\n");
+    if(state == DONE) valid = TRUE;
     //writes to the serial port, trying to connect
     res = writeToSP(fd,buf,SUPERVISION_MESSAGE_SIZE);
     
+    if(valid && res>0){
+        printf("Connected successfully!\n");
+    }
+    else {
+        printf("no connection madwe \n");        
+    }
+   
  
-    printf("Connected successfully!\n");
-    
-    
     sleep(1);
 
     closeSP(fd, &oldtio);
